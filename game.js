91445efc5724ccd526c21b6c0558fb7c2691aad8
Simonsoft12 <31344372+Simonsoft12@users.noreply.github.com>
2021-01-08 11:26:07 +0100

    Add files via upload

---
+++ game.js
@@ -0,0 +1,143 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+canvas.width = 670;
+canvas.height = 600;
+
+var panelX = 300;
+var panelY = 590;
+var panelWidth = 100;
+var panelHeight = 10;
+var panelSpeed = 7;
+
+var ballRadius = 7;
+const BALL_RADIUS = 8;
+var ballX = 350;
+var ballY = 550;
+var speedY = -3;
+var speedX = 3;
+var ballAngle = 0;
+
+var rows = 15;
+var cols = 10;
+
+var brickHeight = 20;
+var brickWidth = 60;
+var brickPadding = 5;
+var brickTopOffset = 10;
+var brickLeftOffset = 15;
+
+var keys = {};
+var bricks = {};
+
+for (i = 0; i < cols; i++) {
+    bricks[i] = {};
+    for (j = 0; j < rows; j++) {
+        bricks[i][j] = { x: 0, y: 0, status: 1}
+    }
+}
+
+for (i = 0; i < cols; i++) {
+    for (j = 0; j < rows; j++) {
+        bricks[i][j].color = 'rgb(' + Math.floor(Math.random() * 256) + ',' + 
+        Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')';
+    }
+}
+
+window.addEventListener('keydown', function (e) {
+    keys[e.keyCode] = true;
+    e.preventDefault();
+});
+
+window.addEventListener('keyup', function (e) {
+    delete keys[e.keyCode];
+});
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(ballX, ballY, ballRadius, 0, BALL_RADIUS);
+    ctx.fillStyle = "darkblue";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPanel() {
+    ctx.beginPath();
+    ctx.rect(panelX, panelY, panelWidth, panelHeight);
+    ctx.fillStyle = 'red';
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawBricks() {
+    for (c = 0; c < cols; c++) {
+        for (r = 0; r < rows; r++) {
+            if (bricks[c][r].status == 1) {
+                var brickX = (c * (brickWidth + brickPadding)) + brickLeftOffset;
+                var brickY = (r * (brickHeight + brickPadding)) + brickTopOffset;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = bricks[c][r].color;
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+function ballPaddleCollision(){
+    if(ballX < panelX + panelWidth && ballX > panelX && panelY < panelY + panelHeight && ballY > panelY){
+        ballAngle = ((panelX + panelWidth / 2) - ballX) * 0.05;
+        speedX = speedX + ballAngle;
+        speedY = -speedY;
+    }
+}
+
+function collisionDetection() {
+    for (c = 0; c < cols; c++) {
+        for (r = 0; r < rows; r++) {
+            var b = bricks[c][r];
+            if (b.status == 1) {
+                if (ballX + BALL_RADIUS > b.x && ballX - BALL_RADIUS < b.x + brickWidth &&
+                     ballY + BALL_RADIUS > b.y && ballY - BALL_RADIUS < b.y + brickHeight) {
+                    speedY = -speedY;
+                    b.status = 0;
+                }
+            }
+        }
+    }
+}
+
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
+    drawPanel();
+    drawBall();
+    collisionDetection();
+    ballPaddleCollision();
+    if(ballX + speedX > canvas.width-ballRadius || ballX + speedX < ballRadius) {
+        speedX = -speedX;
+    }
+    
+    if (37 in keys) {
+        if (panelX - panelSpeed > 0) {
+            panelX -= panelSpeed;
+        }
+    } 
+    else if (39 in keys) {
+        if (panelX + panelWidth + panelSpeed < canvas.width) {
+            panelX += panelSpeed;
+        }
+    }
+    ballX += speedX;
+    ballY += speedY;
+}
+
+function game() {
+    draw();
+    window.requestAnimationFrame(game);
+}
+game();
