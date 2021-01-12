584efcead5d4e5c98875bb8439635b7f384b01db
Simonsoft12 <szymonptak1993@gmail.com>
2021-01-12 14:50:50 +0100

    fixing

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ canvas.height = 600;
 
 var panelX = 300;
 var panelY = 590;
-var panelWidth = 300;
+var panelWidth = 200;
 var panelHeight = 20;
 var panelSpeed = 8;
 
@@ -117,9 +117,9 @@ function handleOrientation(e) {
     var z = e.alpha; // range [0,360], up-down
     
     if(window.innerHeight < window.innerWidth){
-        panelX += y*0.35;
+        if (panelX + panelWidth + panelSpeed < canvas.height) panelX += y*0.35;
     } else {
-        panelX += x*0.35;
+        if (panelX + panelWidth + panelSpeed < canvas.width) panelX += x*0.35;
     }
 }
 
